d4f8b88c77d68353e5892740212aa1d4083ce83a
vj13573 <vj13573@hotmail.com>
2015-02-09 20:03:59 +0000

    Create Command_insult.java

---
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_insult.java
@@ -0,0 +1,7 @@
+package me.StevenLawson.TotalFreedomMod.Commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
