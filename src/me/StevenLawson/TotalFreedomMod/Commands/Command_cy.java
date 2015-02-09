7d9d479e44415ba9c8e41ea96ffc1b2869af484e
vj13573 <vj13573@hotmail.com>
2015-02-09 19:09:53 +0000

    New personal ClearChat!

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_cy.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_cy.java
@@ -6,6 +6,28 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-@CommandPermissions(level = AdminLevel.SUPER, source = SourceType.ONLY_IN_GAME)
-@CommandParameters(description = "Clear the chat.", usage = "/<command>")
+@CommandPermissions(level = AdminLevel.ALL, source = SourceType.ONLY_IN_GAME)
+@CommandParameters(description = "Clear your own chat.", usage = "/<command>")
 public class Command_cy extends TFM_Command
+{
+  @Override
+  public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
+  {
+    if (args.length == 0)
+    {
+      Player player = (Player) sender;
+      
+      for (int i = 0; i <= 100; i++)
+      {
+        sender.sendMessage("");
+      }
+      
+      sender.sendMessage(ChatColor.BLUE + "You have cleared your personal chat.");
+      
+      return true;
+    }
+    
+    return true;
+  }
+  
+}
