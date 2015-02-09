cc7a353c020defd1ab27ff00ccfa9b13a607538e
vj13573 <vj13573@hotmail.com>
2015-02-09 20:14:49 +0000

    Delete Command_insult.java

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_insult.java
+++
@@ -1,7 +0,0 @@
-package me.StevenLawson.TotalFreedomMod.Commands;
-
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
