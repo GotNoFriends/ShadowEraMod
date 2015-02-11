8df3b8bfc235db70a8138254a12c0c47c7a95ce1
VjGamingUK <vj13573@hotmail.com>
2015-02-11 17:28:47 +0000

    Update and rename Command_emote to Command_emote.java

--- src/me/BrandonSteed/ShadowEraMod/Commands/Command_emote.java
+++ src/me/BrandonSteed/ShadowEraMod/Commands/Command_emote.java
@@ -2,9 +2,17 @@ package me.BrandonSteed.TotalFreedomMod.Commands;
 
 import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.GameMode;
 import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+
+@CommandPermissions(level = AdminLevel.ALL, source = SourceType.ONLY_IN_GAME)
+@CommandParameters(description = "Custom Emotes Chat.", usage = "/<command> <emote type> [args]")
+public class Command_gtfo extends TFM_Command
+{
+
+}
